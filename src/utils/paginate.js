0499b454fc3e37711dd84ec66fcf1f64df1f2e76
vakitaki <vakitaki@yandex.com>
2023-04-18 11:37:12 +0300

    shb

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -2,4 +2,3 @@ export const paginate = (items, pageNumber, pageSize) => {
     const startIndex = (pageNumber - 1) * pageSize;
     return [...items].splice(startIndex, pageSize);
 };
-// mdfnnvdfnvjdfnjvn
